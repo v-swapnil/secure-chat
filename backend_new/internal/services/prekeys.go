6d238f0f96c72506352f8f11881208ceda800b56
swapnil <swapnil.suryajoshi@gmail.com>
2025-11-30 19:07:12 +0530

    fix

--- backend_new/internal/services/prekeys.go
+++ backend_new/internal/services/prekeys.go
@@ -1,60 +1,62 @@
 package services
 
 import (
-    "time"
+	"time"
 
-    "github.com/gofrs/uuid"
-    "gorm.io/gorm"
-    "gorm.io/gorm/clause"
+	"github.com/gofrs/uuid"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 
-    "github.com/securechat/backend/internal/models"
+	"github.com/securechat/backend/internal/models"
 )
 
 type PreKeyService struct {
-    DB *gorm.DB
+	DB *gorm.DB
 }
 
 func NewPreKeyService(db *gorm.DB) *PreKeyService {
-    return &PreKeyService{DB: db}
+	return &PreKeyService{DB: db}
 }
 
 func (s *PreKeyService) StoreSignedPreKey(userID uuid.UUID, keyID string, prekey []byte, signature []byte, expires time.Time) error {
-    pk := &models.PreKey{
-        ID:        uuid.Must(uuid.NewV4()),
-        UserID:    userID,
-        KeyID:     keyID,
-        PreKey:    prekey,
-        Signature: signature,
-        ExpiresAt: expires,
-    }
-    return s.DB.Create(pk).Error
+	pk := &models.PreKey{
+		ID:        uuid.Must(uuid.NewV4()),
+		UserID:    userID,
+		KeyID:     keyID,
+		PreKey:    prekey,
+		Signature: signature,
+		ExpiresAt: expires,
+	}
+	return s.DB.Create(pk).Error
 }
 
 func (s *PreKeyService) AddOneTimePreKeys(userID uuid.UUID, keys [][]byte) error {
-    for _, k := range keys {
-        otp := &models.OneTimePreKey{
-            ID:     uuid.Must(uuid.NewV4()),
-            UserID: userID,
-            PreKey: k,
-            Used:   false,
-        }
-        if err := s.DB.Create(otp).Error; err != nil { return err }
-    }
-    return nil
+	for _, k := range keys {
+		otp := &models.OneTimePreKey{
+			ID:     uuid.Must(uuid.NewV4()),
+			UserID: userID,
+			PreKey: k,
+			Used:   false,
+		}
+		if err := s.DB.Create(otp).Error; err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (s *PreKeyService) ConsumeOneTimePreKey(userID uuid.UUID) (*models.OneTimePreKey, error) {
-    var p models.OneTimePreKey
-    tx := s.DB.Begin()
-    if err := tx.Clauses(clause.Locking{Strength: "UPDATE"}).Where("user_id = ? AND used = false", userID).Order("created_at asc").First(&p).Error; err != nil {
-        tx.Rollback()
-        return nil, err
-    }
-    p.Used = true
-    if err := tx.Save(&p).Error; err != nil {
-        tx.Rollback()
-        return nil, err
-    }
-    tx.Commit()
-    return &p, nil
+	var p models.OneTimePreKey
+	tx := s.DB.Begin()
+	if err := tx.Clauses(clause.Locking{Strength: "UPDATE"}).Where("user_id = ? AND used = false", userID).Order("created_at asc").First(&p).Error; err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+	p.Used = true
+	if err := tx.Save(&p).Error; err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+	tx.Commit()
+	return &p, nil
 }
--- backend_new/internal/services/websocket_hub.go
+++ backend_new/internal/services/websocket_hub.go
@@ -1,57 +1,59 @@
 package services
 
 import (
-    "sync"
-    "time"
+	"sync"
+	"time"
 
-    "github.com/gofrs/uuid"
-    "github.com/gofiber/websocket/v2"
+	"github.com/gofiber/websocket/v2"
+	"github.com/gofrs/uuid"
 )
 
 type Connection struct {
-    UserID   uuid.UUID
-    DeviceID string
-    Conn     *websocket.Conn
-    Send     chan []byte
-    LastSeen time.Time
+	UserID   uuid.UUID
+	DeviceID string
+	Conn     *websocket.Conn
+	Send     chan []byte
+	LastSeen time.Time
 }
 
 type Hub struct {
-    mu          sync.RWMutex
-    connections map[uuid.UUID]*Connection
+	mu          sync.RWMutex
+	connections map[uuid.UUID]*Connection
 }
 
 func NewHub() *Hub {
-    return &Hub{
-        connections: make(map[uuid.UUID]*Connection),
-    }
+	return &Hub{
+		connections: make(map[uuid.UUID]*Connection),
+	}
 }
 
 func (h *Hub) Register(c *Connection) {
-    h.mu.Lock()
-    h.connections[c.UserID] = c
-    h.mu.Unlock()
+	h.mu.Lock()
+	h.connections[c.UserID] = c
+	h.mu.Unlock()
 }
 
 func (h *Hub) Unregister(uid uuid.UUID) {
-    h.mu.Lock()
-    if c, ok := h.connections[uid]; ok {
-        close(c.Send)
-        delete(h.connections, uid)
-    }
-    h.mu.Unlock()
+	h.mu.Lock()
+	if c, ok := h.connections[uid]; ok {
+		close(c.Send)
+		delete(h.connections, uid)
+	}
+	h.mu.Unlock()
 }
 
 func (h *Hub) SendTo(userID uuid.UUID, payload []byte) bool {
-    h.mu.RLock()
-    c, ok := h.connections[userID]
-    h.mu.RUnlock()
-    if !ok { return false }
-    select {
-    case c.Send <- payload:
-        return true
-    default:
-        go h.Unregister(userID)
-        return false
-    }
+	h.mu.RLock()
+	c, ok := h.connections[userID]
+	h.mu.RUnlock()
+	if !ok {
+		return false
+	}
+	select {
+	case c.Send <- payload:
+		return true
+	default:
+		go h.Unregister(userID)
+		return false
+	}
 }
